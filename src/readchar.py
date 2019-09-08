157254dd042eca5371afb12feda6cb651551ef0d
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-08 00:48:46 -0400

    reading,printing & replacing characters

---
+++ src/readchar.py
@@ -0,0 +1,49 @@
+print ("girraffe academy")
+
+# print in new line using \n
+
+print ("giraffe \nacademy")
+
+# "\ "is used to print something after it
+
+print ("giraffe\"academy")
+
+# "phrase" is a variable used to store value of a string
+
+phrase = "giraffe academy"
+print(phrase)
+
+# making phrase into lower case
+
+phrase = ("Giraffee Academy")
+print(phrase.lower())
+
+# making phrase into upper case
+
+phrase = ("Giraffee Academy")
+print(phrase.upper())
+
+# isupper = upper true or false
+phrase = ("Giraffee Academy")
+print(phrase.isupper())
+
+# finding the length of the string
+
+phrase = ("Giraffee Academy")
+print(len(phrase))
+
+#finding the character of particular string
+
+phrase = "Giraffe Academy"
+print(phrase[3])
+
+# INDEX = passing a paramter/ giving a value.... to know where the given number letter is
+
+ 
+phrase = "Giraffe Academy"
+print(phrase.index("Academy"))
+
+# REPLACE = replacing a character from the phrase
+
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "elephant"))
