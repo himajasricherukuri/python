071f08f80dd85b060cb10efce049c2b58eb322db
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-09 10:37:23 -0400

    printing numbers

--- src/bookstore.py
+++ src/bookstore.py
@@ -5,15 +5,16 @@ books = input ("Enter books:")
 
 if books == "0":
     print("Number of points earned = 0 ")
-elif books == "1" or books == "2" :
+elif books == "1" or "2" :
     print("Number of points earned = 5")
-elif books == "3" or books == "4" :
+elif books == "3" or "4" :
     print ("Number of points earned = 10")
-elif books == "5" or books == "6" :
-    print ("number of points earned = 15")
-elif books == "7" or books == "8" :
-    print ("number of points earned = 20")
-else :
-    print ("number of points earned = 25")
+elif books == "5" or  "6" :
+    print ("Number of points earned = 15")
+elif books == "7" or "8" :
+    print ("Number of points earned = 20")
+
+else:
+    print ("Number of points earned = 25")
 
 
