da3235c8e1dc023caf97a31fe6a67ec128d3a345
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-09 11:00:11 -0400

    printing numbers

---
+++ src/printnum.py
@@ -0,0 +1,37 @@
+print ( 2 + 3 )
+
+print (2)
+
+#storing a value
+
+my_num =5
+print (my_num)
+
+#converting a number into string
+# printing number next to words,  use STRING (str)function
+my_num = 5
+print(str(my_num) + " my favorite number")
+
+
+#FUNCTIONS FOR NUMBERS IN PYTHON
+
+# 1.)  ABSOLUTE VALUE (abs)
+
+my_num = -5
+print(abs(my_num))
+
+# 2.) POWER (pow)
+
+print (pow(4,5))
+
+# 3.) finding maximum, minimum of two numers (max), (min)
+
+print (max(4,6))
+
+print (min(4,6))
+
+# 4.) round = rounds the number
+
+print (round (3.2))
+
+print (round(3.7))
