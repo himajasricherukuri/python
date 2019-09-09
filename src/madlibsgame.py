e4d5417e83557f46aca1ccf3e9866197765af3b8
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-09 13:28:44 -0400

    create a madlibs game

---
+++ src/madlibsgame.py
@@ -0,0 +1,9 @@
+# madlibs game is basically just a game where you can enter in a bunch od random words so like verbs, nouns, names, colors
+# we take all these names and but them into story
+color = input ("Enter a color:")
+plural_noun = input("Enter a plural noun:")
+celebrity = input("Enter a celebrity:")
+
+print("Roses are" = color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
