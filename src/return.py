d224051b53ad60c83324f9bd0a51e89d37ed4125
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-10 23:08:03 -0400

    returns a value

---
+++ src/return.py
@@ -0,0 +1,13 @@
+# return keyword - returns a particular statement / value to the function
+
+def cube(num):
+    return num*num*num
+
+print(cube(3))
+
+# result 
+def cube(num):
+     return num*num*num
+
+result = cube(4)
+print(result)
