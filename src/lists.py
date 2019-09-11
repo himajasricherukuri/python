fe27de62606c42327fb1512c81903dce1d3bbdf9
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-10 21:29:31 -0400

    creating a list

---
+++ src/lists.py
@@ -0,0 +1,22 @@
+# lists-structure used in python to store lists of information
+
+#put a bunch of data in list and u can use them throughtout the program
+
+# 1. give the list a name
+# [] - represents stroring bunch of values
+
+friends =  ["kevin","karen","jim"]
+
+# what to put in a list ? = strings, numbers, booleans
+
+#how to access elements in a list
+
+print(friends)
+
+#how to access specific element ?
+
+print(friends[-1])
+
+#how to aceess multiple elements ? = use ":" after the element number
+
+print(friends[1:])
