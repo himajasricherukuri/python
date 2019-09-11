034d04399c8169dcb4fa4a88c069b6a05067925a
Himaja Sri Cherukuri <chhimajasri@gmail.com>
2019-09-11 14:48:20 -0400

    simple program using while loop

---
+++ src/while.py
@@ -0,0 +1,11 @@
+# while loop helps us to execute a block of code multiple times
+
+# 1. create an integer
+
+i = 1
+while i <= 10: # loop condition
+    print(i)
+    i += 1     # += adds 1 to the corresponding number
+
+
+print ("Done with the loop")
